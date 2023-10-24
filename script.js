7dfb44886d509f299585bfbd276f45a765cda956
Bilel Hamitouche <billelmadridista470@gmail.com>
2023-10-24 23:27:44 +0100

    Making the calculator work and modifying the UI.

--- script.js
+++ script.js
@@ -1,39 +1,88 @@
 
+// storing all buttons 
+const buttons = document.querySelectorAll('button');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const previous = document.querySelector('.previous');
+const current = document.querySelector('.current');
+const equal = document.querySelector('.equal');
+const clear = document.querySelector('.clear');
 
-// add
-const add = function(a, b) {
+let displayValue = '';
+
+// operation functions
+const add = (a, b) => {
     return a + b;
-};
+}
 
-// subtract
-const subtract = function(a, b) {
+const subtract = (a, b) => {
     return a - b;
-};
+}
 
-// multiply
-const multiply = function(a, b) {
+const multiply = (a, b) => {
     return a * b;
-};
+}
 
-// divide
-const divide = function(a, b) {
-    return b ? a / b : 'Error, division by zero'; 
-};
+const divide = (a, b) => {
+    if (b === '0') return 'oops';
+    return a / b;
+}
 
-// operate
-const operate = function(operator, num1, num2) {
-    switch (operator) {
-        case add:
-	    add(num1, num2);
-	    break;
-        case subtract:
-	    subtract(num1, num2);
-	    break;
-        case multiply:
-	    multiply(num1, num2);
-	    break;
-        case divide:
-	    divide(num1, num2);
-	    break;
+// operate function
+const operate = (operation, number1, number2) => {
+    switch (operation) {
+    case '+':
+	return add(number1, number2);
+	break;
+    case '-':
+	return subtract(number1, number2);
+	break;
+    case 'x':
+	return multiply(number1, number2);
+	break;
+    case '/':
+	return divide(number1, number2);
+	break;
     }
 }
+
+// event listeners for numbers
+numbers.forEach((num) => {
+    num.addEventListener('click', () => {
+	current.textContent += num.textContent;
+	displayValue += num.textContent;
+    })
+})
+
+// event listeners for operators
+operators.forEach((opr) => {
+    opr.addEventListener('click', () => {
+	if (previous.textContent.includes('+') || previous.textContent.includes('-') || previous.textContent.includes('x') || previous.textContent.includes('/') && previous.textContent.indexOf('-') !== 0) {
+	    previous.textContent = operate(previous.textContent[previous.textContent.length - 1], previous.textContent.slice(0, -1), current.textContent) + opr.textContent;
+	} else {
+	    previous.textContent = current.textContent + opr.textContent;
+	}
+	current.textContent = '';
+    })
+})
+
+// event listener for clear
+clear.addEventListener('click', () => {
+    previous.textContent = '';
+    current.textContent = '';
+})
+
+equal.addEventListener('click', () => {
+    displayValue = operate(previous.textContent[previous.textContent.length - 1], previous.textContent.slice(0, -1), current.textContent);
+    current.textContent = displayValue;
+})
+
+// change background of button when hovering
+buttons.forEach((btn) => {
+    btn.addEventListener('mouseover', () => {
+	btn.style.backgroundColor = '#dddddd'; 
+    })
+    btn.addEventListener('mouseout', () => {
+	btn.style.backgroundColor = '#ffffff'; 
+    })
+})
